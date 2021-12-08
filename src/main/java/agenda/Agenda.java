7ede82ef9732164a3f1803400f546aeb26d3f299
ClotildeSalvador <clotilde.salvador@etud.univ-fc.fr>
2021-12-08 12:19:40 +0100

    constructeurs terminatedEvent modifiés

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -9,33 +9,33 @@ import java.util.*;
  * Description : An agenda that stores events
  */
 public class Agenda {
-    /**
-     * Adds an event to this agenda
-     *
-     * @param e the event to add
-     */
-	
-	private ArrayList<Event> listEvent ;
-	
-    public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
-        listEvent.add(e) ;
-    }
+	/**
+	 * Adds an event to this agenda
+	 *
+	 * @param e the event to add
+	 */
 
-    /**
-     * Computes the events that occur on a given day
-     *
-     * @param day the day toi tes
-     * @return and iteraror to the events that occur on that day
-     */
-    public List<Event> eventsInDay(LocalDate day) {
-    	List<Event> list = new ArrayList<Event>() ;
-        // TODO : implémenter cette méthode
-        for (Event e : this.listEvent) {
-        	if (e.isInDay(day)) {
-        		list.add(e);
-        	}
-        }
-        return list ;
-    }
+	private ArrayList<Event> listEvent;
+
+	public void addEvent(Event e) {
+		// TODO : implémenter cette méthode
+		listEvent.add(e);
+	}
+
+	/**
+	 * Computes the events that occur on a given day
+	 *
+	 * @param day the day toi tes
+	 * @return and iteraror to the events that occur on that day
+	 */
+	public List<Event> eventsInDay(LocalDate day) {
+		List<Event> list = new ArrayList<Event>();
+		// TODO : implémenter cette méthode
+		for (Event e : this.listEvent) {
+			if (e.isInDay(day)) {
+				list.add(e);
+			}
+		}
+		return list;
+	}
 }
