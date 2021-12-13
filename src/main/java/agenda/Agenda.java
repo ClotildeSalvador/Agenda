32e9f0eb8635d78a67d286b29c0e1cf748f1c650
ClotildeSalvador <clotilde.salvador@etud.univ-fc.fr>
2021-12-13 16:54:54 +0100

    methode isfreefor

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -1,5 +1,6 @@
 package agenda;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -9,43 +10,6 @@ import java.util.*;
  * Description : An agenda that stores events
  */
 public class Agenda {
-<<<<<<< HEAD
-    /**
-     * Adds an event to this agenda
-     *
-     * @param e the event to add
-     */
-	
-	private List<Event> listEvent = new ArrayList();
-	
-    public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
-        listEvent.add(e) ;
-    }
-
-    /**
-     * Computes the events that occur on a given day
-     *
-     * @param day the day toi tes
-     * @return and iteraror to the events that occur on that day
-     */
-   /* public List<Event> eventsInDay(LocalDate day) {
-    	List<Event> list = new ArrayList<Event>() ;
-        // TODO : implémenter cette méthode
-        for (Event e : this.listEvent) {
-        	if (e.isInDay(day)) {
-                    list.add(e);
-        	}
-                if (e.getDuration()
-                    
-            
-                    
-                }
-                
-        }
-        return list ;
-    }*/
-=======
 	/**
 	 * Adds an event to this agenda
 	 *
@@ -75,5 +39,55 @@ public class Agenda {
 		}
 		return list;
 	}
->>>>>>> 7ede82ef9732164a3f1803400f546aeb26d3f299
+	
+	/**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) {
+    	boolean r = true ;
+    	
+//    	date de l'evenement
+    	int y = e.getStart().getYear() ;
+    	int m = e.getStart().getMonthValue();
+    	int d = e.getStart().getDayOfMonth();
+    	LocalDate l = LocalDate.of(y, m, d) ;
+//    	heure de l'evenement
+    	int h = e.getStart().getHour() ;
+    	int min = e.getStart().getMinute() ;
+    	LocalTime t = LocalTime.of(h, min) ;
+//    	ev l'evenement existant auquel on compare l'evenement e
+        for (Event ev : this.eventsInDay(l)) {
+//        	recupere l'heure de l'evenement
+        	int hev = ev.getStart().getHour() ;
+        	int minev = ev.getStart().getMinute() ;
+        	LocalTime tev = LocalTime.of(hev, minev) ;
+//        	si le debut de ev est avant celui de e, on regarde si la fin de ev est avant le debut de e
+        	if (tev.isBefore(t)) {
+//        		duree de l'evenement ev existant
+        		Duration dev = ev.getDuration() ;
+        		LocalTime tevfin=tev.plus(dev) ;
+        		if(tevfin.isAfter(t)) {
+        			r=false ;
+        		}
+        	}
+//        	si meme nb de minutes et meme heure = meme temps de debut : r=false
+        	if (hev==h && minev==min) {
+        		r=false ;
+        	}
+//        	si le debut de ev est apres celui de e, on regarde si la fin de e est avant le debut de ev
+        	if (tev.isAfter(t)) {
+//        		duree de l'evenement ev existant
+        		Duration de = e.getDuration() ;
+        		LocalTime tfin=tev.plus(de) ;
+        		if(tfin.isAfter(tev)) {
+        			r=false ;
+        		}
+        	}
+        	
+        }
+        return r ;
+    }
+
 }
--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -8,7 +8,7 @@ import java.time.temporal.ChronoUnit;
  * Description : A repetitive Event
  */
 public class RepetitiveEvent extends Event {
-<<<<<<< HEAD
+
     /**
      * Constructs a repetitive event
      *
@@ -25,11 +25,14 @@ public class RepetitiveEvent extends Event {
 	
 	private ChronoUnit frequency ;
 	
+	private List<LocalDate> lesExceptions ; 
+	
 	
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
         // TODO : implémenter cette méthode
         this.frequency=frequency ;
+        this.lesExceptions = new ArrayList<>() ;
        
     }
 
@@ -39,8 +42,7 @@ public class RepetitiveEvent extends Event {
      * @param date the event will not occur at this date
      */
     public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    	lesExceptions.add(date) ;
     }
 
     /**
@@ -67,48 +69,5 @@ public class RepetitiveEvent extends Event {
             return "0";
         }
     }
-=======
-	/**
-	 * Constructs a repetitive event
-	 *
-	 * @param title     the title of this event
-	 * @param start     the start of this event
-	 * @param duration  myDuration in seconds
-	 * @param frequency one of :
-	 *                  <UL>
-	 *                  <LI>ChronoUnit.DAYS for daily repetitions</LI>
-	 *                  <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
-	 *                  <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
-	 *                  </UL>
-	 */
-
-	private ChronoUnit frequency;
-
-	public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
-		super(title, start, duration);
-		// TODO : implémenter cette méthode
-		this.frequency = frequency;
-
-	}
-
-	/**
-	 * Adds an exception to the occurrence of this repetitive event
-	 *
-	 * @param date the event will not occur at this date
-	 */
-	public void addException(LocalDate date) {
-		// TODO : implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
-
-	/**
-	 *
-	 * @return the type of repetition
-	 */
-	public ChronoUnit getFrequency() {
-		// TODO : implémenter cette méthode
-		return this.frequency;
-	}
->>>>>>> 7ede82ef9732164a3f1803400f546aeb26d3f299
 
 }
